1f27f42a4de0ce2da20536b168826dc755bd02b0
Adi Levy-Alalouf <blue.vixen@gmail.com>
2021-11-03 14:01:22 +0200

    updated API

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -7,6 +7,7 @@ export const mapService = {
 }
 
 var gMap;
+const API_KEY = 'AIzaSyDNyrNr0MyFXTxYobEUYROPnvMr9rrvxCA';
 
 function initMap(lat = 32.0749831, lng = 34.9120554) {
     console.log('InitMap');
