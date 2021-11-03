ea5f12f956deb05a5bb3a1851bbd07be957dfb91
Yaniv Richman <yaniv4980@gmail.com>
2021-11-03 14:57:23 +0200

    change to normal

--- js/app.controller.js
+++ js/app.controller.js
@@ -18,7 +18,6 @@ function onInit() {
 // This function provides a Promise API to the callback-based-api of getCurrentPosition
 function getPosition() {
     console.log('Getting Pos');
-    console.log('Adi')
     return new Promise((resolve, reject) => {
         navigator.geolocation.getCurrentPosition(resolve, reject)
     })
@@ -26,7 +25,6 @@ function getPosition() {
 
 function onAddMarker() {
     console.log('Adding a marker');
-    console.log('Yaniv');
     mapService.addMarker({ lat: 32.0749831, lng: 34.9120554 });
 }
 
